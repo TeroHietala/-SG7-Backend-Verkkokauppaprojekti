628c7edf1ed777de1e01a0e08b97f865d8d9e23b
Pietari Tanner <cotapi01@students.oamk.fi>
2021-11-29 19:20:18 +0200

    search lisätty

---
+++ products/searchproducts.php
@@ -0,0 +1,15 @@
+<?php
+require_once '../inc/functions.php';
+require_once '../inc/headers.php';
+
+$url = parse_url(filter_input(INPUT_SERVER,'PATH_INFO'),PHP_URL_PATH);
+$parameters = explode('/',$url);
+$category_id = $parameters[1];
+
+try {
+    $db = openDb();
+    selectAsJson($db, "select * from product");
+} 
+catch (PDOException $pdoex) {
+    returnError($pdoex);
+}
